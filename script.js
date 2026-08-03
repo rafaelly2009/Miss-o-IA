601a697ae84a0d736e02697071a08c6bb1593acb
rafaelly2009 <silva.andrade.rafaelly@escola.pr.gov.br>
2026-08-03 08:22:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Ao sair da escola, Davi vê um parque cheio de lixo e pens no impacto disso para a naturea. O que ele decide?",
+    alternativas: [
+        "Cuidar do meio ambiente é responsabilidade de todos.",
+        "Pequenas atitudes não fazem, diferença para o meio ambiente."
+    ]
+},
+{
+    enunciado: "Depois de aprender sobre a preservação da natureza, a professora pede que Davi proponha uma ação para reduzir o lixo na escola. Qual atitude Davi toma?",
+    alternativas: [
+        "Organiza uma campanha de reciclagem com a turma.",
+        "Decide não participar da campanha porque acredita que não fará dirença."
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+];
